38c9673e1ceb10e38366f6eb4a068e7ea9474f88
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 15:07:29 +0530

    end point bundle edited

--- src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
+++ src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
@@ -5,4 +5,10 @@ public final class  EndpointBundle {
     public static final String ID ="/{id}";
     public static final String SEARCH = "/search";
 
+    public static final String SETTINGS = BASE_URL + "/settings";
+    public static final String HOTEL_ID = "/{hotelId}";
+    public static final String ROLE = "/roles";
+    public static final String CREATE_ROLE = HOTEL_ID+ROLE+"/add";
+
+
 }
