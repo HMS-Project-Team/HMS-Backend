78d05a812d341e7163e1de0bd97ce27010adb9ae
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-08 15:51:09 +0530

    Updated AuthenticationService

--- src/main/java/com/example/hms/HMS/services/AuthenticationService.java
+++ src/main/java/com/example/hms/HMS/services/AuthenticationService.java
@@ -1,4 +1,8 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.requests.NewPasswordRequestDto;
+
 public interface AuthenticationService {
+
+    void newPasswordCheck(NewPasswordRequestDto newPasswordRequestDto);
 }
