9da5d3c92ad73e8838f0e7619f44d9e29bda073f
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-08 15:45:19 +0530

    Updated SecurityConfig

---
+++ src/main/java/com/example/hms/HMS/exceptionHandlers/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.example.hms.HMS.exceptionHandlers;
+
+public class IllegalArgumentException extends RuntimeException {
+  public IllegalArgumentException(String message) {
+    super(message);
+  }
+}
