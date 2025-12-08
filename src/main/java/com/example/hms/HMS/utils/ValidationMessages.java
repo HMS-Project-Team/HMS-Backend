264fa3ea8872e79fac95f98c1a3601d50e378762
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-08 15:53:37 +0530

    Updated ValidationMessages

--- src/main/java/com/example/hms/HMS/utils/ValidationMessages.java
+++ src/main/java/com/example/hms/HMS/utils/ValidationMessages.java
@@ -34,6 +34,10 @@ public class ValidationMessages {
     public static final String INVALID_EMAIL = "Invalid email format.";
     public static final String PASSWORD_REQUIRED = "Password is required.";
     public static final String PASSWORD_MIN_LENGTH = "Password must be at least 8 characters.";
+    public static final String PASSWORD_COMPLEXITY = "Password must contain uppercase, lowercase, number, and special character";
+    public static final String PASSWORD_MISMATCH = "New password and confirm password do not match";
+    public static final String NEW_PASSWORD_SAME_AS_OLD = "New password cannot be the same as the old password";
+
 
     // ==== OTP ====
     public static final String OTP_NOT_FOUND = "OTP not found.";
