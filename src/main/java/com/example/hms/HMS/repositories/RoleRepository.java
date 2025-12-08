e048db2ef197659f1cd101532ee6852bfbbbf1ce
kiruththiga <kiruthikakulasegaram@gmail.com>
2025-12-08 10:54:56 +0530

    Role Repository Added

--- src/main/java/com/example/hms/HMS/repositories/RoleRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.example.hms.HMS.repositories;
 
 import com.example.hms.HMS.entities.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Page<Role> findByHotelId(Long hotelId, Pageable pageable);
 }
