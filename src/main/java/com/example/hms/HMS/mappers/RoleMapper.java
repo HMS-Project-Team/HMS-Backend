e84be0aeabd5853e75f239eec48b3fec226d857b
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 15:06:40 +0530

    role mapper edited

--- src/main/java/com/example/hms/HMS/mappers/RoleMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/RoleMapper.java
@@ -1,12 +1,19 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.requests.RoleRequestDto;
+import com.example.hms.HMS.dtos.responses.RoleResponseDto;
 import com.example.hms.HMS.entities.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 
+    @Mapping(source = "hotel.id", target="hotelId")
+    RoleResponseDto toResDto(Role role);
+
+    Role toEntity(RoleRequestDto roleRequestDto);
 
 }
