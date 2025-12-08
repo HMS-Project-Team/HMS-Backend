042c4f165d549caf743cfe0d43852cb10399cf02
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-08 15:58:01 +0530

    get all ResponeDto

--- src/main/java/com/example/hms/HMS/dtos/responses/HotelResponseDto.java
+++ src/main/java/com/example/hms/HMS/dtos/responses/HotelResponseDto.java
@@ -1,4 +1,19 @@
 package com.example.hms.HMS.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HotelResponseDto {
+    private Long id;
+    private String hotelName;
+    private String address;
+    private String city;
+    private String country;
+    private String phoneNumber;
+    private String website;
+    private String email;
 }
