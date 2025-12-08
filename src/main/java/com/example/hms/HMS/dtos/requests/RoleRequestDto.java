eda5b45e41103e26e9ed1ad2a54291e054247325
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 15:05:23 +0530

    RoleRequestDto created

--- src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
@@ -1,4 +1,17 @@
 package com.example.hms.HMS.dtos.requests;
 
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class RoleRequestDto {
+
+    @NotBlank(message = ValidationMessages.REQUIRED_FIELD_MISSING)
+    private String name;
+    private Long hotelId;
 }
