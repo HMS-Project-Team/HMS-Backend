374f8f73578f62e31ee0041b75fc398ac2dfcb2b
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 15:52:50 +0530

    HotelRequestDto created

--- src/main/java/com/example/hms/HMS/dtos/requests/HotelRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/HotelRequestDto.java
@@ -1,4 +1,17 @@
 package com.example.hms.HMS.dtos.requests;
 
+import jakarta.persistence.Lob;
+import lombok.Data;
+
+@Data
 public class HotelRequestDto {
+
+    private String hotelName;
+    private String address;
+    private String city;
+    private String country;
+    private String phoneNumber;
+    private String website;
+    private byte[] logoImage;
+    private String email;
 }
