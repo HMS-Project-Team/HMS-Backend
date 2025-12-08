92e972346beca45368484ef3f507be6ae93e9178
Hany11-11 <rs11hari@gmail.com>
2025-12-08 21:43:34 +0530

    UserService added

--- src/main/java/com/example/hms/HMS/services/UserService.java
+++ src/main/java/com/example/hms/HMS/services/UserService.java
@@ -1,4 +1,8 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.requests.UserRequestDto;
+import com.example.hms.HMS.dtos.responses.UserResponseDto;
+
 public interface UserService {
+    UserResponseDto getUserById(Long id);
 }
