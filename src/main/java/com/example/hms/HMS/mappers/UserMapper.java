e22d9daf8acfa90407b794fefcdcf3a22ae08b03
Hany11-11 <rs11hari@gmail.com>
2025-12-08 21:43:16 +0530

    UserMapper added

--- src/main/java/com/example/hms/HMS/mappers/UserMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/UserMapper.java
@@ -1,9 +1,14 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.responses.UserResponseDto;
+import com.example.hms.HMS.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
+    @Mapping(target = "roles", ignore = true)
+    @Mapping(target = "hotelId", ignore = true)
+    UserResponseDto toResponseDto(User user);
 }
