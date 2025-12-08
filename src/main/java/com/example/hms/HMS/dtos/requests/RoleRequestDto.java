7a9316bc1fa395ff40670fd5580dce5d7f366fe6
unknown <vijayapremanath@gmail.com>
2025-12-08 11:38:19 +0530

    update RoleRequestDto

--- src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
@@ -1,4 +1,10 @@
 package com.example.hms.HMS.dtos.requests;
 
+import lombok.Data;
+
+@Data
 public class RoleRequestDto {
+    private Long id;
+    private String name;
+    private Long hotelId;
 }
