d6204ad207c6119c80399bd4eb7d8db5d4c6056b
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 15:53:11 +0530

    HotelMapper edited

--- src/main/java/com/example/hms/HMS/mappers/HotelMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/HotelMapper.java
@@ -1,5 +1,7 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.requests.HotelRequestDto;
+import com.example.hms.HMS.dtos.responses.HotelResponseDto;
 import com.example.hms.HMS.entities.Hotel;
 import org.mapstruct.Mapper;
 
@@ -7,6 +9,9 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface HotelMapper {
+
+    Hotel toEntity(HotelRequestDto hotelRequestDto);
+    HotelResponseDto toDto(Hotel hotel);
 
 
 }
