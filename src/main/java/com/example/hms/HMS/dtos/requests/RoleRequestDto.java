e216c3f099e7bd5d974b731014557a5ea651e6cd
Hany11-11 <rs11hari@gmail.com>
2025-12-08 09:31:01 +0530

    role request dto updated

--- src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
@@ -1,4 +1,18 @@
 package com.example.hms.HMS.dtos.requests;
 
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleRequestDto {
+    private Long id;
+    @NotBlank(message = ValidationMessages.REQUIRED_FIELD_MISSING)
+    private String name;
+    private Long hotelId;
 }
