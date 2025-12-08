43fc7cf8dae1cda5707cb4c35b623117a56f648b
Hany11-11 <rs11hari@gmail.com>
2025-12-08 10:50:45 +0530

    Endpoint changed

--- src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
+++ src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
@@ -9,6 +9,7 @@ public final class  EndpointBundle {
     public static final String SETTINGS = BASE_URL + "/settings";
 
     //Roles
-    public static final String ROLES = SETTINGS + "/roles";
-
+    public static final String ROLES = "/roles";
+    public static final String ROLES_BY_ID = ROLES + ID;
+    public static final String ROLES_BY_HOTEL = "/{hotelId}" + ROLES;
 }
