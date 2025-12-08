a29bf4ceacadf2d269349e1d838235ae5ac906b4
Hany11-11 <rs11hari@gmail.com>
2025-12-08 13:59:32 +0530

    RoleRequestDto edited

--- src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/RoleRequestDto.java
@@ -2,7 +2,6 @@ package com.example.hms.HMS.dtos.requests;
 
 import com.example.hms.HMS.utils.ValidationMessages;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
