8e9f954d4956621eabfd2274b62f2b8ae0badc8c
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 14:13:15 +0530

    EndpointBundle edited

--- src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
+++ src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
@@ -5,4 +5,9 @@ public final class  EndpointBundle {
     public static final String ID ="/{id}";
     public static final String SEARCH = "/search";
 
+    public static final String SETTINGS = BASE_URL + "/settings";
+    public static final String USER = "/users";
+    public static final String DELETE_USER = USER+ID;
+
+
 }
