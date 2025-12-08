0e3a09e1191d68167bbef7842ce1c180ed945094
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-08 15:58:55 +0530

    get all HotelService

--- src/main/java/com/example/hms/HMS/services/HotelService.java
+++ src/main/java/com/example/hms/HMS/services/HotelService.java
@@ -1,6 +1,12 @@
 package com.example.hms.HMS.services;
 
 
+import com.example.hms.HMS.dtos.responses.HotelResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface HotelService {
     boolean deleteHotel(Long id);
+
+    Page<HotelResponseDto> getHotels(Pageable pageable);
 }
