d4bf973a39f8d0230fc42a6edeb6297792835f27
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-08 16:50:44 +0530

    Add EmailServiceImpl

--- src/main/java/com/example/hms/HMS/services/EmailServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/EmailServiceImpl.java
@@ -1,4 +1,27 @@
 package com.example.hms.HMS.services;
 
-public class EmailServiceImpl {
+import com.example.hms.HMS.entities.User;
+import com.example.hms.HMS.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void sendOtpEmail(String recipientEmail, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(recipientEmail);
+        message.setSubject("Your Otp Code");
+        message.setText("Your OTP for HMS verification is: " + otp);
+        mailSender.send(message);
+    }
+
+
+
 }
