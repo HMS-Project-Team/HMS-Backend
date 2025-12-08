9604b126c6b92fe207e1f143f7a1bf960ed915e4
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-08 15:59:19 +0530

    get all HotelService

--- src/main/java/com/example/hms/HMS/services/HotelServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/HotelServiceImpl.java
@@ -1,15 +1,24 @@
 package com.example.hms.HMS.services;
 
 
+import com.example.hms.HMS.dtos.responses.HotelResponseDto;
+import com.example.hms.HMS.entities.Hotel;
+import com.example.hms.HMS.exceptionHandlers.InvalidPageSizeException;
 import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.mappers.HotelMapper;
 import com.example.hms.HMS.repositories.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HotelServiceImpl implements HotelService {
     @Autowired
     private HotelRepository hotelRepository;
+
+    @Autowired
+    private HotelMapper hotelMapper;
 
     @Override
     public boolean deleteHotel(Long id) {
@@ -19,4 +28,23 @@ public class HotelServiceImpl implements HotelService {
         hotelRepository.deleteById(id);
         return true;
     }
+
+    @Override
+    public Page<HotelResponseDto> getHotels(Pageable pageable) {
+
+        if (pageable.getPageNumber() < 0 || pageable.getPageSize() <= 0) {
+            throw new InvalidPageSizeException("Invalid Page or Size value");
+        }
+
+        Page<Hotel> hotelPage = hotelRepository.findAll(pageable);
+
+        if (hotelPage.isEmpty()) {
+            throw new ResourceNotFoundException("No Hotels found");
+        }
+
+        // Convert to DTO using MapStruct
+        return hotelPage.map(hotelMapper::toResponseDto);
+    }
+
+
 }
