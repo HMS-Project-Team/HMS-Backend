776b57e97af4a20623015637e46774dff2503703
kiruththiga <kiruthikakulasegaram@gmail.com>
2025-12-08 23:13:18 +0530

    User controller added

--- src/main/java/com/example/hms/HMS/controllers/UserController.java
+++ src/main/java/com/example/hms/HMS/controllers/UserController.java
@@ -1,4 +1,35 @@
 package com.example.hms.HMS.controllers;
 
+import com.example.hms.HMS.dtos.requests.UserRequestDto;
+import com.example.hms.HMS.dtos.responses.UserResponseDto;
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
+import com.example.hms.HMS.services.UserService;
+import com.example.hms.HMS.utils.EndpointBundle;
+import com.example.hms.HMS.utils.ResponseWrapper;
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(EndpointBundle.SETTINGS)
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @PutMapping(EndpointBundle.USERS_BY_ID)
+    public ResponseEntity<ResponseWrapper<UserResponseDto>> updateUser(
+            @PathVariable Long id,@Valid
+            @RequestBody UserRequestDto requestDto) {
+
+        UserResponseDto updatedUser = userService.updateUser(id, requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseWrapper<>(
+                        RestApiResponseStatusCodes.OK.getCode(),
+                        ValidationMessages.UPDATED_SUCCESSFULLY,
+                        updatedUser));
+    }
+
 }
