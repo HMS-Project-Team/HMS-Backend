f836797ee0888d99b11fbc2ba49496232aba8684
Hany11-11 <rs11hari@gmail.com>
2025-12-08 21:56:30 +0530

    UserRequestDto changed

--- src/main/java/com/example/hms/HMS/dtos/requests/UserRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/UserRequestDto.java
@@ -1,6 +1,28 @@
 package com.example.hms.HMS.dtos.requests;
 
-public class UserRequestDto {
 
-    private Long id;
+import com.example.hms.HMS.entities.Role;
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDto {
+    private String firstname;
+    private String lastname;
+    @Email(message = ValidationMessages.INVALID_EMAIL)
+    private String email;
+    private String phone;
+    private String address;
+    private String NIC;
+    private String country;
+    private String city;
+    private String password;
+    private List<Long> roles;
 }
