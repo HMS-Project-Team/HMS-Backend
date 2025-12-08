36dcc9398dfed115995a4227526d15bf454685e6
kiruththiga <kiruthikakulasegaram@gmail.com>
2025-12-08 23:15:42 +0530

    user response dto added

--- src/main/java/com/example/hms/HMS/dtos/responses/UserResponseDto.java
+++ src/main/java/com/example/hms/HMS/dtos/responses/UserResponseDto.java
@@ -1,4 +1,26 @@
 package com.example.hms.HMS.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UserResponseDto {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String address;
+    private String NIC;
+    private String country;
+    private String city;
+    private Long hotelId;
+    private List<Long> roles;
+
 }
+
