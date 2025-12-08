c66d3504b919cc3589a51b8b689035f2e8a69575
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-08 16:50:59 +0530

    Get all hotel End bundle and REsponseDto

--- src/main/java/com/example/hms/HMS/dtos/responses/HotelResponseDto.java
+++ src/main/java/com/example/hms/HMS/dtos/responses/HotelResponseDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HotelResponseDto {
-
     private Long id;
     private String hotelName;
     private String address;
