bb68af6ad1bf23de95620d8b2c326e480484210c
kiruththiga <kiruthikakulasegaram@gmail.com>
2025-12-08 10:55:39 +0530

    role service added

--- src/main/java/com/example/hms/HMS/services/RoleService.java
+++ src/main/java/com/example/hms/HMS/services/RoleService.java
@@ -1,4 +1,9 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.responses.RoleResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface RoleService {
+ Page<RoleResponseDto> GetAllRoles(Long hotelId, Pageable pageable);
 }
