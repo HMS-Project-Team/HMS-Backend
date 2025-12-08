f5a4822ecfef2fca76d6ecc008d46ac3e9b9eee5
unknown <vijayapremanath@gmail.com>
2025-12-08 11:40:55 +0530

    Updated RoleServiceImpl

--- src/main/java/com/example/hms/HMS/services/RoleServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/RoleServiceImpl.java
@@ -1,4 +1,28 @@
 package com.example.hms.HMS.services;
 
-public class RoleServiceImpl {
+import com.example.hms.HMS.entities.Role;
+import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.repositories.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void deleteRole(Long roleId) {
+        Role role = roleRepository.findById(roleId)
+                .orElseThrow(() -> new ResourceNotFoundException("Role not found with id: " + roleId));
+
+        if (role.getUsers() != null) {
+            role.getUsers().forEach(user -> user.getRoles().remove(role));
+            role.getUsers().clear();
+        }
+
+        roleRepository.delete(role);
+    }
+
+
 }
