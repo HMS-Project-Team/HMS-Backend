7e4bc67b3ee3d02809ee1f6a386f037d8694afb7
IT24100618 <it24100618@my.sliit.lk>
2025-12-08 15:55:32 +0530

    hotel service created

---
+++ src/main/java/com/example/hms/HMS/services/HotelService.java
@@ -0,0 +1,10 @@
+package com.example.hms.HMS.services;
+
+import com.example.hms.HMS.dtos.requests.HotelRequestDto;
+import com.example.hms.HMS.dtos.responses.HotelResponseDto;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+
+public interface HotelService {
+    HotelResponseDto updateHotel(Long id, HotelRequestDto hotelRequestDto) throws HttpRequestMethodNotSupportedException;
+
+}
