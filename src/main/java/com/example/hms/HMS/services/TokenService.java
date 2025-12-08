027f2ebaf3765d73f674f7a90716ad8f7d18a8bc
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-08 16:50:56 +0530

    Add TokenService

--- src/main/java/com/example/hms/HMS/services/TokenService.java
+++ src/main/java/com/example/hms/HMS/services/TokenService.java
@@ -1,4 +1,15 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.requests.TokenRequestDto;
+import com.example.hms.HMS.entities.Token;
+import com.example.hms.HMS.entities.User;
+
 public interface TokenService {
+    Token createTokenForUser(User user, int otp, int expiryMinutes);
+    void saveOtpForUser(User user, int otp);
+
+    TokenRequestDto getOtpForUser(User user);
+
+    void revokeOtp(User user);
+
 }
