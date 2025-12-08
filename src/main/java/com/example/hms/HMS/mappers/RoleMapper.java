0cbda5fe3b7172e3c7452389c02f4af40513421a
unknown <vijayapremanath@gmail.com>
2025-12-08 11:38:47 +0530

    update RoleMapper

--- src/main/java/com/example/hms/HMS/mappers/RoleMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/RoleMapper.java
@@ -1,12 +1,19 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.requests.RoleRequestDto;
 import com.example.hms.HMS.entities.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 
+    @Mapping(source="hotelId",target="hotel.id")
+    Role toEntity(RoleRequestDto dto);
+
+    @Mapping(source="hotel.id",target="hotelId")
+    RoleRequestDto toDto(Role role);
 
 }
