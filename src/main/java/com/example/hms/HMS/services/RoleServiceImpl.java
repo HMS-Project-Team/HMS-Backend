9c1ad5c35bacbcdb1efb096378dd5a5db94acb8a
kiruththiga <kiruthikakulasegaram@gmail.com>
2025-12-08 10:56:32 +0530

    Role Service Impl Added

--- src/main/java/com/example/hms/HMS/services/RoleServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/RoleServiceImpl.java
@@ -1,4 +1,55 @@
 package com.example.hms.HMS.services;
 
-public class RoleServiceImpl {
+import com.example.hms.HMS.dtos.responses.RoleResponseDto;
+import com.example.hms.HMS.entities.Hotel;
+import com.example.hms.HMS.entities.Role;
+import com.example.hms.HMS.exceptionHandlers.InvalidPageSizeException;
+import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.mappers.RoleMapper;
+import com.example.hms.HMS.repositories.RoleRepository;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+    private final EntityManager entityManager;
+
+    @Override
+    public Page<RoleResponseDto> GetAllRoles(Long hotelId, Pageable pageable) {
+
+        int page = pageable.getPageNumber();
+        int size = pageable.getPageSize();
+
+        if (page < 0 || size <= 0) {
+            throw new InvalidPageSizeException(
+                    "Invalid pagination values! page=" + page + ", size=" + size +
+                            ". Page must be >= 0 and size > 0"
+            );
+        }
+
+        Hotel hotel = entityManager.find(Hotel.class, hotelId);
+        if (hotel == null) {
+            throw new ResourceNotFoundException("Hotel not found with id: " + hotelId);
+        }
+
+        Page<Role> rolePage = roleRepository.findByHotelId(hotelId, pageable);
+
+        if (rolePage.isEmpty()) {
+            throw new ResourceNotFoundException(
+                    "No roles found for hotel " + hotelId
+            );
+        }
+
+        return rolePage.map(roleMapper::toDto);
+    }
 }
+
