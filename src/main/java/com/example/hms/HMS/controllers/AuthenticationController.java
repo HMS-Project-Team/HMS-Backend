a3cd05f171a243e53757c7724f6faf28610491de
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-08 21:28:57 +0530

    Updated AuthenticationController

--- src/main/java/com/example/hms/HMS/controllers/AuthenticationController.java
+++ src/main/java/com/example/hms/HMS/controllers/AuthenticationController.java
@@ -5,25 +5,15 @@ import com.example.hms.HMS.dtos.responses.AuthenticationResponseDto;
 import com.example.hms.HMS.services.AuthenticationService;
 import com.example.hms.HMS.dtos.requests.NewPasswordRequestDto;
 import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
-import com.example.hms.HMS.services.AuthenticationServiceImpl;
 import com.example.hms.HMS.utils.EndpointBundle;
-import com.example.hms.HMS.utils.ErrorDetail;
 import com.example.hms.HMS.utils.ResponseWrapper;
-import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
-
-
 import com.example.hms.HMS.utils.ValidationMessages;
+
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(EndpointBundle.AUTH)
@@ -31,26 +21,36 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationService authenticationService;
-    private AuthenticationServiceImpl authenticationService;
 
-    @PutMapping
-    public ResponseEntity<ResponseWrapper<String>> newPassword(@Valid @RequestBody NewPasswordRequestDto newPasswordRequestDto){
+    //New Password
+    @PutMapping(EndpointBundle.NEW_PASSWORD)
+    public ResponseEntity<ResponseWrapper<String>> newPassword(
+            @Valid @RequestBody NewPasswordRequestDto newPasswordRequestDto) {
+
         authenticationService.newPasswordCheck(newPasswordRequestDto);
 
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseWrapper<>(
+                        RestApiResponseStatusCodes.OK.getCode(),
+                        ValidationMessages.UPDATED_SUCCESSFULLY,
+                        null
+                )
+        );
+    }
+
+    //Login
     @PostMapping(EndpointBundle.LOGIN)
-    public ResponseEntity<ResponseWrapper<AuthenticationResponseDto>> login(@Valid @RequestBody LoginDto request) {
+    public ResponseEntity<ResponseWrapper<AuthenticationResponseDto>> login(
+            @Valid @RequestBody LoginDto request) {
+
         AuthenticationResponseDto response = authenticationService.login(request);
 
         ResponseWrapper<AuthenticationResponseDto> wrapper = new ResponseWrapper<>(
                 RestApiResponseStatusCodes.OK.getCode(),
                 RestApiResponseStatusCodes.OK.getMessage(),
-                response);
+                response
+        );
 
         return ResponseEntity.ok(wrapper);
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper<>(
-                RestApiResponseStatusCodes.OK.getCode() ,
-                ValidationMessages.UPDATED_SUCCESSFULLY ,
-                null
-        ));
     }
 }
