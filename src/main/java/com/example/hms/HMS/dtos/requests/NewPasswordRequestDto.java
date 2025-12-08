17eef41ff47a215d3674ca3c46f1881a1ee6d674
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-08 15:47:26 +0530

    Updated NewPasswordRequestDto

--- src/main/java/com/example/hms/HMS/dtos/requests/NewPasswordRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/NewPasswordRequestDto.java
@@ -1,4 +1,25 @@
 package com.example.hms.HMS.dtos.requests;
 
-public class newPasswordRequestDto {
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewPasswordRequestDto {
+    @NotBlank(message = ValidationMessages.EMAIL_REQUIRED)
+    @Email(message = ValidationMessages.INVALID_EMAIL)
+    private String email;
+
+    @NotBlank(message = ValidationMessages.PASSWORD_REQUIRED)
+    @Size(min = 8 , max = 20 , message = ValidationMessages.PASSWORD_MIN_LENGTH)
+    private String newPassword;
+
+    @NotBlank(message = ValidationMessages.PASSWORD_REQUIRED)
+    private String confirmPassword;
 }
