be095f060f7106c6fc09fe5a2517c183e150cb6b
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-08 16:53:28 +0530

    Update TokenRepository

--- src/main/java/com/example/hms/HMS/repositories/TokenRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/TokenRepository.java
@@ -1,9 +1,16 @@
 package com.example.hms.HMS.repositories;
 
 import com.example.hms.HMS.entities.Token;
+import com.example.hms.HMS.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
+    List<Token> findByUserAndRevokedFalseAndType(User user, String type);
+
+    Token findTopByUserIdAndTypeOrderByCreatedAtDesc(Long userId, String type);
+
 }
