8e35ea0da6476c0450454de2b58e74affad49ced
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-08 14:06:05 +0530

    HotelController

---
+++ src/main/java/com/example/hms/HMS/controllers/HotelController.java
@@ -0,0 +1,49 @@
+package com.example.hms.HMS.controllers;
+
+
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
+import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.services.HotelService;
+import com.example.hms.HMS.utils.ResponseWrapper;
+import com.example.hms.HMS.utils.ValidationMessages;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/hotel")
+public class HotelController {
+    @Autowired
+    private HotelService hotelService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseWrapper<Boolean>> deleteHotel(@PathVariable Long id){
+
+        boolean isDeleted = hotelService.deleteHotel(id);
+
+        if (isDeleted){
+            return ResponseEntity.ok(
+                    new ResponseWrapper<>(
+                            RestApiResponseStatusCodes.NO_CONTENT.getCode(),
+                            ValidationMessages.DELETED_SUCCESSFULLY,
+                            true
+                    )
+            );
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ResponseWrapper<>(
+                        RestApiResponseStatusCodes.NOT_FOUND.getCode(),
+                        ValidationMessages.NOT_FOUND,
+                        false
+                )
+        );
+
+    }
+}
