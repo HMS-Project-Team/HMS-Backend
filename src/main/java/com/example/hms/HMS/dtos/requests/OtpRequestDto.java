da933aac614b1841f91bd37f69d4874ab8680bf2
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-08 16:50:16 +0530

    Add OtpRequestDto

--- src/main/java/com/example/hms/HMS/dtos/requests/OtpRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/OtpRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.hms.HMS.dtos.requests;
 
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class OtpRequestDto {
+    @NotBlank(message = ValidationMessages.EMAIL_REQUIRED)
+    @Email(message = ValidationMessages.INVALID_EMAIL)
+    private String email;
 }
