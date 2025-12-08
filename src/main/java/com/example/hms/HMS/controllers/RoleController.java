29614aba0b359afba47f558d13604145e04f697a
Hany11-11 <rs11hari@gmail.com>
2025-12-08 09:39:03 +0530

    RoleController created

---
+++ src/main/java/com/example/hms/HMS/controllers/RoleController.java
@@ -0,0 +1,30 @@
+package com.example.hms.HMS.controllers;
+
+import com.example.hms.HMS.dtos.requests.RoleRequestDto;
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
+import com.example.hms.HMS.services.RoleService;
+import com.example.hms.HMS.utils.EndpointBundle;
+import com.example.hms.HMS.utils.ResponseWrapper;
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(EndpointBundle.ROLES)
+public class RoleController {
+    @Autowired
+    private RoleService roleService;
+
+    @PutMapping(EndpointBundle.ID)
+    public ResponseEntity<ResponseWrapper<RoleRequestDto>> roleUpdate(@PathVariable Long id, @Valid @RequestBody RoleRequestDto roleRequestDto) {
+        RoleRequestDto updated = roleService.updateRole(id, roleRequestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper<>(
+                RestApiResponseStatusCodes.OK.getCode(),
+                ValidationMessages.UPDATED_SUCCESSFULLY,
+                updated));
+    }
+}
