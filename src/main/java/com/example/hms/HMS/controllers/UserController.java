328e94026c4bf4fc30c36e52a82d00f43aae72cb
Hany11-11 <rs11hari@gmail.com>
2025-12-08 21:42:21 +0530

    UserController edited

--- src/main/java/com/example/hms/HMS/controllers/UserController.java
+++ src/main/java/com/example/hms/HMS/controllers/UserController.java
@@ -1,4 +1,26 @@
 package com.example.hms.HMS.controllers;
 
+import com.example.hms.HMS.dtos.requests.UserRequestDto;
+import com.example.hms.HMS.dtos.responses.UserResponseDto;
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
+import com.example.hms.HMS.services.UserService;
+import com.example.hms.HMS.utils.EndpointBundle;
+import com.example.hms.HMS.utils.ResponseWrapper;
+import com.example.hms.HMS.utils.ValidationMessages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(EndpointBundle.SETTINGS)
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(EndpointBundle.USERS_BY_ID)
+    public ResponseEntity<ResponseWrapper<UserResponseDto>> getUser(@PathVariable Long id){
+        UserResponseDto user = userService.getUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper<>(RestApiResponseStatusCodes.OK.getCode(), ValidationMessages.RETRIEVED_SUCCESSFULLY,user));
+    }
 }
