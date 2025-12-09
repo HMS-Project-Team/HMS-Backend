2cb1ba6ba7ad97749a23b28328e9995def6d4e4a
Delojan1808 <121038216+Delojan1808@users.noreply.github.com>
2025-12-09 15:14:46 +0530

    Updated VerifyOtpRequestDto

--- src/main/java/com/example/hms/HMS/dtos/requests/VerifyOtpRequestDto.java
+++ src/main/java/com/example/hms/HMS/dtos/requests/VerifyOtpRequestDto.java
@@ -6,9 +6,12 @@ import lombok.Data;
 
 @Data
 public class VerifyOtpRequestDto {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
-    @NotNull(message= ValidationMessages.OTP_NOTNULL)
-//    @Size(min = 6, max = 6, message = ValidationMessages.OTP_SIZE)
+    @NotNull(message = ValidationMessages.OTP_NOTNULL)
+    @Size(min = 6, max = 6, message = ValidationMessages.OTP_SIZE)
+    @Pattern(regexp = "\\d{6}", message = "OTP must be exactly 6 digits")
     private String otp;
 }
