55177ee02fbf5dc12ce92282718d7fe3dececcce
kiruththiga <kiruthikakulasegaram@gmail.com>
2025-12-09 14:15:15 +0530

    UserMapper  merged

--- src/main/java/com/example/hms/HMS/mappers/UserMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/UserMapper.java
@@ -4,21 +4,13 @@ import com.example.hms.HMS.dtos.requests.UserRequestDto;
 import com.example.hms.HMS.dtos.responses.UserResponseDto;
 import com.example.hms.HMS.entities.Role;
 import com.example.hms.HMS.entities.User;
-import com.example.hms.HMS.dtos.responses.UserResponseDto;
-import com.example.hms.HMS.entities.User;
-import com.example.hms.HMS.dtos.requests.UserRequestDto;
-import com.example.hms.HMS.entities.Role;
-import com.example.hms.HMS.entities.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import java.lang.annotation.Target;
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -52,5 +44,4 @@ public interface UserMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "roles", ignore = true)
     void updateEntity(UserRequestDto dto, @MappingTarget User user);
-}
 }
