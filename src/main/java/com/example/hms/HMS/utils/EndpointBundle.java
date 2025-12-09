e03300f637fc5d669603840bddd691a747b6ebce
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-09 09:38:48 +0530

    Updated EndpointBundle

--- src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
+++ src/main/java/com/example/hms/HMS/utils/EndpointBundle.java
@@ -4,15 +4,12 @@ public final class  EndpointBundle {
     public static final String BASE_URL = "/api/v1";
     public static final String ID ="/{id}";
     public static final String SEARCH = "/search";
-    public static final String AUTH = BASE_URL + "/auth";
-    // Login
-    public static final String LOGIN = "/login";
-
-    public static final String NEW_PASSWORD = "/new-Password";
 
     //Login
+    public static final String LOGIN = "/login";
     public static final String AUTH = BASE_URL + "/auth";
     public static final String OTP = "/otp";
     public static final String VERIFY_OTP = OTP + "/verify";
+    public static final String NEW_PASSWORD = "/new-Password";
 
 }
