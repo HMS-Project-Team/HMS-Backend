b0ce8404d46289e5edfc4456d7a8d8f17ebba1fc
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-09 11:03:25 +0530

    Updated HotelServiceImpl

--- src/main/java/com/example/hms/HMS/services/HotelServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/HotelServiceImpl.java
@@ -1,15 +1,24 @@
 package com.example.hms.HMS.services;
 
 
+import com.example.hms.HMS.dtos.responses.HotelResponseDto;
+import com.example.hms.HMS.entities.Hotel;
 import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.mappers.HotelMapper;
 import com.example.hms.HMS.repositories.HotelRepository;
+import com.example.hms.HMS.utils.ValidationMessages;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class HotelServiceImpl implements HotelService {
-    @Autowired
-    private HotelRepository hotelRepository;
+
+    private final HotelRepository hotelRepository;
+
+
+    private final HotelMapper hotelMapper;
 
     @Override
     public boolean deleteHotel(Long id) {
@@ -19,4 +28,11 @@ public class HotelServiceImpl implements HotelService {
         hotelRepository.deleteById(id);
         return true;
     }
+
+    @Override
+    public HotelResponseDto getHotelById(Long id) {
+            Hotel hotel = hotelRepository.findById(id)
+                    .orElseThrow(()-> new ResourceNotFoundException(ValidationMessages.NOT_FOUND));
+            return hotelMapper.toHotelDto(hotel);
+        }
 }
