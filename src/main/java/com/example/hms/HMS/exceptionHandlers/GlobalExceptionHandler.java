893e426712f558f0ee8e7fae802d7d00e42fb531
unknown <vijayapremanath@gmail.com>
2025-12-09 08:59:21 +0530

    insert InvalidPageSizeException

--- src/main/java/com/example/hms/HMS/exceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/example/hms/HMS/exceptionHandlers/GlobalExceptionHandler.java
@@ -201,6 +201,16 @@ public class GlobalExceptionHandler {
         );
     }
 
+    @ExceptionHandler({InvalidPageSizeException.class})
+    public ResponseEntity<ResponseWrapper<?>> handleInvalidPageSize(InvalidPageSizeException ex) {
+        ErrorDetail d = new ErrorDetail(new Date(), ex.getMessage(), errorCodes.getPaginationInvalid());
+        return ResponseEntity.badRequest().body(new ResponseWrapper<>(
+                RestApiResponseStatusCodes.BAD_REQUEST.getCode(),
+                InvalidPageSizeException.INVALID_PAGE_SIZE_MSG,
+                d
+        ));
+    }
+
 
 
 
