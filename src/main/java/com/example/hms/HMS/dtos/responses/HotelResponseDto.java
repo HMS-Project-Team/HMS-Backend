b11ceda0168cca7a58a8368dddaf4c54adcabb02
unknown <vijayapremanath@gmail.com>
2025-12-09 08:57:47 +0530

    updated HotelResponseDto

--- src/main/java/com/example/hms/HMS/dtos/responses/HotelResponseDto.java
+++ src/main/java/com/example/hms/HMS/dtos/responses/HotelResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.hms.HMS.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HotelResponseDto {
+    private Long id;
+    private String hotelName;
+
 }
