7954053743f8e0b326cc50bf73598967cf03b3e2
unknown <vijayapremanath@gmail.com>
2025-12-09 08:59:53 +0530

    Insert InvalidPageSizeException

--- src/main/java/com/example/hms/HMS/exceptionHandlers/InvalidPageSizeException.java
+++ src/main/java/com/example/hms/HMS/exceptionHandlers/InvalidPageSizeException.java
@@ -1,7 +1,9 @@
 package com.example.hms.HMS.exceptionHandlers;
 
 public class InvalidPageSizeException extends RuntimeException {
-  public InvalidPageSizeException(String message) {
-    super(message);
-  }
+    public static final String INVALID_PAGE_SIZE_MSG = "Invalid Page Size.";
+
+    public InvalidPageSizeException(String message) {
+        super(message);
+    }
 }
