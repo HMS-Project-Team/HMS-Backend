d895f840f993b47c8bf7f28af9c64b54c8d8d27d
unknown <vijayapremanath@gmail.com>
2025-12-09 09:01:36 +0530

    UserService

--- src/main/java/com/example/hms/HMS/services/UserService.java
+++ src/main/java/com/example/hms/HMS/services/UserService.java
@@ -1,4 +1,10 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.responses.UserResponseDto;
+import com.example.hms.HMS.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface UserService {
+    Page<UserResponseDto> getAllUsers(Long hotelId, int page, int size);
 }
