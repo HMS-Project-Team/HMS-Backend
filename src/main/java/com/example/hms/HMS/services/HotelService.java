03c4e84d2db1e09203753b9cdca9fd5cdcb8b884
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-09 11:03:02 +0530

    Updated HotelService

--- src/main/java/com/example/hms/HMS/services/HotelService.java
+++ src/main/java/com/example/hms/HMS/services/HotelService.java
@@ -1,6 +1,10 @@
 package com.example.hms.HMS.services;
 
 
+import com.example.hms.HMS.dtos.responses.HotelResponseDto;
+
 public interface HotelService {
     boolean deleteHotel(Long id);
+
+    HotelResponseDto getHotelById(Long id);
 }
