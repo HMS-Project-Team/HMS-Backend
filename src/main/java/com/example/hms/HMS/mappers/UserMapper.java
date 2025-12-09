5e3281c24d9f17b981d09d93a0ad93c5ba0068f9
unknown <vijayapremanath@gmail.com>
2025-12-09 09:00:37 +0530

    UserMapper

--- src/main/java/com/example/hms/HMS/mappers/UserMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/UserMapper.java
@@ -1,9 +1,33 @@
 package com.example.hms.HMS.mappers;
 
+
+import com.example.hms.HMS.dtos.responses.RoleResponseDto;
+import com.example.hms.HMS.dtos.responses.UserResponseDto;
+import com.example.hms.HMS.entities.Role;
+import com.example.hms.HMS.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "hotelId", expression = "java(user.getRoles() != null && !user.getRoles().isEmpty() ? user.getRoles().get(0).getHotel().getId() : null)")
+    @Mapping(source = "roles", target = "roles", qualifiedByName = "rolesToIds")
+    UserResponseDto toDto(User user);
+
+    @Mapping(target = "roles", ignore = true)
+    User toEntity(UserResponseDto userDto);
+
+    @Named("rolesToIds")
+    default List<Long> rolesToIds(List<Role> roles) {
+        if (roles == null) return null;
+        return roles.stream().map(Role::getId).collect(Collectors.toList());
+    }
+
+    List<UserResponseDto> toDtoList(List<User> users);
 }
