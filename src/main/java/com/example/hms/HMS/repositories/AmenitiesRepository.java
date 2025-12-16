4441b168689bc7d0cd8c6b111fa54588fed9d670
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-16 17:51:31 +0530

    Updated AmenitiesRepository

--- src/main/java/com/example/hms/HMS/repositories/AmenitiesRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/AmenitiesRepository.java
@@ -2,6 +2,8 @@ package com.example.hms.HMS.repositories;
 
 import com.example.hms.HMS.entities.Amenities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AmenitiesRepository extends JpaRepository<Amenities , Long> {
 }
