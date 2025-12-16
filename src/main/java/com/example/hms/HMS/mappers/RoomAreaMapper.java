9535471b7b06d43fdf9b46764255e9edfb07820a
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-16 15:46:10 +0530

    Updated RoomAreaMapper

--- src/main/java/com/example/hms/HMS/mappers/RoomAreaMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/RoomAreaMapper.java
@@ -1,4 +1,12 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.requests.RoomAreaRequestDto;
+import com.example.hms.HMS.dtos.responses.RoomAreaResponseDto;
+import com.example.hms.HMS.entities.RoomArea;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface RoomAreaMapper {
+    RoomAreaResponseDto toResponseDto(RoomArea roomArea);
+    RoomArea toEntity(RoomAreaRequestDto requestDto);
 }
