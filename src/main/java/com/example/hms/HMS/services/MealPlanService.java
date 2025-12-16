f2504227ae92f2e1792c2fec676f2d2152a2c91e
Hany11-11 <rs11hari@gmail.com>
2025-12-16 17:06:06 +0530

    MealPlanService edited

--- src/main/java/com/example/hms/HMS/services/MealPlanService.java
+++ src/main/java/com/example/hms/HMS/services/MealPlanService.java
@@ -1,10 +1,13 @@
 package com.example.hms.HMS.services;
 
 import com.example.hms.HMS.dtos.responses.MealPlanResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 
 public interface MealPlanService {
     MealPlanResponseDto GetByIdMealplan(Long id);
     boolean deleteMealplan(Long id) throws HttpRequestMethodNotSupportedException;
+    Page<MealPlanResponseDto> getMealPlans(Pageable pageable);
 
 }
