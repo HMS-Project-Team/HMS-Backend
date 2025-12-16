5322648bac5c7f840d6f0490846862aa8da7f342
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-16 21:44:05 +0530

    Amenities Edit Api

--- src/main/java/com/example/hms/HMS/controllers/AmenitiesController.java
+++ src/main/java/com/example/hms/HMS/controllers/AmenitiesController.java
@@ -89,12 +89,9 @@ public class AmenitiesController {
                 amenitiesResponseDto));
     }
     @PutMapping(EndpointBundle.ID)
-    public ResponseEntity<ResponseWrapper<AmenitiesResponseDto>> updateAmenities(
-            @PathVariable Long id,
-            @Valid @RequestBody AmenitiesRequestDto amenitiesRequestDto) {
+    public ResponseEntity<ResponseWrapper<AmenitiesResponseDto>> updateAmenities(@PathVariable Long id, @Valid @RequestBody AmenitiesRequestDto amenitiesRequestDto) {
 
-        AmenitiesResponseDto responseDto =
-                amenitiesService.updateAmenities(id, amenitiesRequestDto);
+        AmenitiesResponseDto responseDto = amenitiesService.updateAmenities(id, amenitiesRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(
                 new ResponseWrapper<>(
