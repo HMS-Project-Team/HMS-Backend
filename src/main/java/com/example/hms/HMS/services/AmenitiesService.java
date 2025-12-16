b2b8d86b4c2ec2bf733429e6e41169bb7621b2de
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-16 17:51:53 +0530

    Updated AmenitiesService

--- src/main/java/com/example/hms/HMS/services/AmenitiesService.java
+++ src/main/java/com/example/hms/HMS/services/AmenitiesService.java
@@ -3,7 +3,9 @@ package com.example.hms.HMS.services;
 import com.example.hms.HMS.dtos.responses.AmenitiesResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AmenitiesService{
     Page<AmenitiesResponseDto> fetchAllAmenities(Pageable pageable);
 
