defc8601cec4a9650ae0d69e49dcdfe21572d35b
unknown <vijayapremanath@gmail.com>
2025-12-16 15:45:08 +0530

    create RoomAreaRepository

--- src/main/java/com/example/hms/HMS/repositories/RoomAreaRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/RoomAreaRepository.java
@@ -1,4 +1,8 @@
 package com.example.hms.HMS.repositories;
 
-public interface RoomAreaRepository {
+import com.example.hms.HMS.entities.RoomArea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomAreaRepository extends JpaRepository<RoomArea,Long> {
+    boolean existsByName(String name);
 }
