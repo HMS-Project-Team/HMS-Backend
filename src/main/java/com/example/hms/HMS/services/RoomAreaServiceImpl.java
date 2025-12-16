d2b2f90eed271f53eed92e4b64002bbaf43a2d33
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-16 15:48:22 +0530

    Updated RoomRoomAreaServiceImpl

--- src/main/java/com/example/hms/HMS/services/RoomAreaServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/RoomAreaServiceImpl.java
@@ -1,4 +1,29 @@
 package com.example.hms.HMS.services;
 
-public class RoomAreaServiceImpl {
+import com.example.hms.HMS.dtos.requests.RoomAreaRequestDto;
+import com.example.hms.HMS.dtos.responses.RoomAreaResponseDto;
+import com.example.hms.HMS.entities.RoomArea;
+import com.example.hms.HMS.mappers.RoomAreaMapper;
+import com.example.hms.HMS.repositories.RoomAreaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoomAreaServiceImpl implements RoomAreaService{
+
+    private final RoomAreaMapper roomAreaMapper;
+    private final RoomAreaRepository roomAreaRepository;
+
+    @Override
+    public RoomAreaResponseDto updateRoomArea(Long id, RoomAreaRequestDto roomAreaRequestDto) {
+        try{
+            RoomArea roomArea = roomAreaMapper.toEntity(roomAreaRequestDto);
+            roomArea.setId(id);
+            RoomArea updated = roomAreaRepository.save(roomArea);
+            return roomAreaMapper.toResponseDto(updated);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Missing Field");
+        }
+    }
 }
