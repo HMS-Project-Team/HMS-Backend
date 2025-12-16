188ea6300e99bc10dfb0d6ba934c9359aafcecb6
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-16 17:30:36 +0530

    Updated AmenitiesMapper

--- src/main/java/com/example/hms/HMS/mappers/AmenitiesMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/AmenitiesMapper.java
@@ -1,4 +1,12 @@
 package com.example.hms.HMS.mappers;
 
-public interface AmenitiesMappaer {
+import com.example.hms.HMS.dtos.responses.AmenitiesResponseDto;
+import com.example.hms.HMS.entities.Amenities;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+@Mapper(componentModel = "spring")
+public interface AmenitiesMapper {
+
+    AmenitiesResponseDto toDto (Amenities amenities);
 }
