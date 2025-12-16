9c4dfbfd38952f61359f86e1edf13011341a2a81
unknown <vijayapremanath@gmail.com>
2025-12-16 15:45:58 +0530

    create RoomAreaService

--- src/main/java/com/example/hms/HMS/services/RoomAreaService.java
+++ src/main/java/com/example/hms/HMS/services/RoomAreaService.java
@@ -1,4 +1,9 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.requests.RoomAreaRequestDto;
+import com.example.hms.HMS.dtos.responses.RoomAreaResponseDto;
+
 public interface RoomAreaService {
+
+    RoomAreaResponseDto createRoomArea(RoomAreaRequestDto roomAreaRequestDto);
 }
