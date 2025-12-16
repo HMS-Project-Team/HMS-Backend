40e75b3a4132fd77fce8ed66d4543d7d513aa304
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-16 17:30:03 +0530

    Updated AmenitiesController

---
+++ src/main/java/com/example/hms/HMS/services/AmenitiesServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.hms.HMS.services;
+
+import com.example.hms.HMS.dtos.responses.AmenitiesResponseDto;
+import com.example.hms.HMS.entities.Amenities;
+import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.mappers.AmenitiesMapper;
+import com.example.hms.HMS.repositories.AmenitiesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AmenitiesService {
+
+   
+}
