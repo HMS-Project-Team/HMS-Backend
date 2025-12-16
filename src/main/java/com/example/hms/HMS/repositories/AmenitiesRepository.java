df08835d89473ba616bb48229f12b92ef42d097f
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-16 18:59:19 +0530

    Updated AmenitiesRepository

--- src/main/java/com/example/hms/HMS/repositories/AmenitiesRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/AmenitiesRepository.java
@@ -1,4 +1,14 @@
 package com.example.hms.HMS.repositories;
 
-public interface AmenitiesRepository {
+
+import com.example.hms.HMS.entities.Amenities;
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AmenitiesRepository extends JpaRepository<Amenities, Long> {
+
+    boolean findByNameOrIcon(@NotBlank(message = ValidationMessages.REQUIRED_FIELD_MISSING) @Pattern(regexp = "^[A-Za-z ]+$",
+            message = ValidationMessages.INVALID_INPUT) String name, @NotBlank(message = ValidationMessages.REQUIRED_FIELD_MISSING) @Pattern(regexp = "^[A-Za-z_]",message = ValidationMessages.INVALID_FORMAT) String icon);
 }
