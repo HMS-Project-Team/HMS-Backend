8415f40bad2a75d9055fb879c0f76455a4093310
Hany11-11 <rs11hari@gmail.com>
2025-12-16 15:07:44 +0530

    RoomAreaMapper added

---
+++ src/main/java/com/example/hms/HMS/mappers/RoomAreaMapper.java
@@ -0,0 +1,10 @@
+package com.example.hms.HMS.mappers;
+
+import com.example.hms.HMS.dtos.responses.RoomAreaResponseDto;
+import com.example.hms.HMS.entities.RoomArea;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface RoomAreaMapper {
+    RoomAreaResponseDto toResponseDto(RoomArea roomArea);
+}
