cbf4c2d1ac1e287f577a6d64ce94a22f80844e89
Hany11-11 <rs11hari@gmail.com>
2025-12-16 15:10:01 +0530

    RoomAreaService added

---
+++ src/main/java/com/example/hms/HMS/services/RoomAreaService.java
@@ -0,0 +1,7 @@
+package com.example.hms.HMS.services;
+
+import com.example.hms.HMS.dtos.responses.RoomAreaResponseDto;
+
+public interface RoomAreaService {
+    RoomAreaResponseDto getRoomArea(Long id);
+}
