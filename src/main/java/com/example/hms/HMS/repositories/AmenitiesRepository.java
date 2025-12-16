9d3bc08e96fe2dfa974f9227b74ac1629e4c2403
Delojan1808 <121038216+Delojan1808@users.noreply.github.com>
2025-12-16 19:05:17 +0530

    create amenities repository

---
+++ src/main/java/com/example/hms/HMS/repositories/AmenitiesRepository.java
@@ -0,0 +1,11 @@
+package com.example.hms.HMS.repositories;
+
+import com.example.hms.HMS.entities.Amenities;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AmenitiesRepository extends JpaRepository<Amenities, Long> {
+
+    boolean existsByNameOrIcon(String name,String icon);
+}
