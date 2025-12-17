ac886afdcb2d3d132b1eb71102b38adf92493edc
Delojan1808 <121038216+Delojan1808@users.noreply.github.com>
2025-12-17 09:54:41 +0530

    import class

--- src/main/java/com/example/hms/HMS/services/ViewTypeService.java
+++ src/main/java/com/example/hms/HMS/services/ViewTypeService.java
@@ -2,6 +2,10 @@ package com.example.hms.HMS.services;
 
 import com.example.hms.HMS.dtos.requests.ViewTypeRequestDto;
 import com.example.hms.HMS.dtos.responses.ViewTypeResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ViewTypeService {
     ViewTypeResponseDto createViewType(ViewTypeRequestDto viewTypeRequestDto);
