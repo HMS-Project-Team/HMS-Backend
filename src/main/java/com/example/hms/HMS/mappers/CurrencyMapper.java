f077a29f49bc59fe6897aeee0b66849578f2a20b
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-22 15:29:55 +0530

    Updated CurrencyMapper

--- src/main/java/com/example/hms/HMS/mappers/CurrencyMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/CurrencyMapper.java
@@ -1,7 +1,10 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.responses.CurrencyResponseDto;
+import com.example.hms.HMS.entities.Currency;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CurrencyMapper {
+    CurrencyResponseDto toResponseDto (Currency currency);
 }
