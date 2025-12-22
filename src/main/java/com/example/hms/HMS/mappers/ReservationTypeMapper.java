71d81f4d5ec4666c171e7e68ed3c9b98bb6af2d9
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-22 16:50:25 +0530

    Updated ReservationTypeMapper

--- src/main/java/com/example/hms/HMS/mappers/ReservationTypeMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/ReservationTypeMapper.java
@@ -1,7 +1,11 @@
 package com.example.hms.HMS.mappers;
 
+import com.example.hms.HMS.dtos.responses.ReservationTypeResponseDto;
+import com.example.hms.HMS.entities.ReservationType;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ReservationTypeMapper {
+
+    ReservationTypeResponseDto toResponseDto (ReservationType reservationType);
 }
