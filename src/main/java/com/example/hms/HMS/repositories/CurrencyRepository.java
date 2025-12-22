b7a4e4cb38574ad4c5306d8433a7c24d7fe8858b
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-22 15:30:46 +0530

    Updated CurrencyRepository

--- src/main/java/com/example/hms/HMS/repositories/CurrencyRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/CurrencyRepository.java
@@ -4,6 +4,10 @@ import com.example.hms.HMS.entities.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CurrencyRepository extends JpaRepository <Currency , Long> {
+    boolean existsByCurrencyName(String currencyName);
+    boolean existsByCode(String code);
 }
