1d9fee0e350a3a9fa06999abca4109a7fb5cb2ee
unknown <vijayapremanath@gmail.com>
2025-12-22 15:32:44 +0530

    GuestsController

--- src/main/java/com/example/hms/HMS/controllers/GuestsController.java
+++ src/main/java/com/example/hms/HMS/controllers/GuestsController.java
@@ -1,7 +1,12 @@
 package com.example.hms.HMS.controllers;
 
+import com.example.hms.HMS.dtos.responses.GuestsResponseDto;
+import com.example.hms.HMS.services.GuestsService;
 import com.example.hms.HMS.utils.EndpointBundle;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(EndpointBundle.GUESTS)
 @RequiredArgsConstructor
 public class GuestsController {
+
+    private final GuestsService guestsService;
+
+    @GetMapping(EndpointBundle.ID)
+    public ResponseEntity<GuestsResponseDto> getGuestsById(@PathVariable Long id){
+        GuestsResponseDto response = guestsService.getGuestsById(id);
+        return ResponseEntity.ok(response);
+    }
+
 }
