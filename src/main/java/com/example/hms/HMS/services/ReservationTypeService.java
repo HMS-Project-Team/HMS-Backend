35139746dac24b4367029fb9fcd3016d69aad439
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-22 16:50:47 +0530

    Updated ReservationTypeService

--- src/main/java/com/example/hms/HMS/services/ReservationTypeService.java
+++ src/main/java/com/example/hms/HMS/services/ReservationTypeService.java
@@ -1,4 +1,11 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.responses.ReservationTypeResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
 public interface ReservationTypeService {
+
+    Page<ReservationTypeResponseDto> fetchAllReservationType(Pageable pageSize);
 }
