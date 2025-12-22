0fffdbe1206c0f7889d675ac1788f0a187ea7e9f
unknown <vijayapremanath@gmail.com>
2025-12-22 15:34:02 +0530

    GuestsServiceImpl

--- src/main/java/com/example/hms/HMS/services/GuestsServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/GuestsServiceImpl.java
@@ -1,9 +1,26 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.responses.GuestsResponseDto;
+import com.example.hms.HMS.entities.Guests;
+import com.example.hms.HMS.mappers.GuestsMapper;
+import com.example.hms.HMS.repositories.GuestsRepository;
+import jdk.jshell.spi.ExecutionControl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class GuestsServiceImpl implements GuestsService{
+
+    private final GuestsRepository guestsRepository;
+    private final GuestsMapper guestsMapper;
+
+
+    @Override
+    public GuestsResponseDto getGuestsById(long id){
+        Guests guest =guestsRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("\"Guest not found with id: " + id));
+
+        return guestsMapper.toEntity(guest) ;
+    }
 }
