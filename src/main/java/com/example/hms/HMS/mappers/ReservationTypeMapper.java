879f5e071303c2696d913b40aa67bf77a8eda4f1
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-22 16:58:30 +0530

    Updated ReservationTypeMapper

--- src/main/java/com/example/hms/HMS/mappers/ReservationTypeMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/ReservationTypeMapper.java
@@ -3,8 +3,6 @@ package com.example.hms.HMS.mappers;
 import com.example.hms.HMS.dtos.requests.ReservationTypeRequestDto;
 import com.example.hms.HMS.dtos.responses.ReservationTypeResponseDto;
 import com.example.hms.HMS.entities.ReservationType;
-import com.example.hms.HMS.dtos.responses.ReservationTypeResponseDto;
-import com.example.hms.HMS.entities.ReservationType;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
