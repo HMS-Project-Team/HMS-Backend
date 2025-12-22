703048c8bbfc08dace79cfc6e76e1f79baa4c34a
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-22 17:37:56 +0530

    Updated GuestsService

--- src/main/java/com/example/hms/HMS/services/GuestsService.java
+++ src/main/java/com/example/hms/HMS/services/GuestsService.java
@@ -1,8 +1,11 @@
 package com.example.hms.HMS.services;
 
 import com.example.hms.HMS.dtos.responses.GuestsResponseDto;
+import jakarta.validation.Valid;
 
 public interface GuestsService {
 
     GuestsResponseDto getGuestsById(long id);
+
+    boolean deleteManageGuests(@Valid Long id);
 }
