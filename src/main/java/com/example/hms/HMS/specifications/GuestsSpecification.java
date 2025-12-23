47e3c9330f7fc45804298eaaa0c588414eb7764c
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-23 06:16:30 +0530

    GuestsSpecification 2

--- src/main/java/com/example/hms/HMS/specifications/GuestsSpecification.java
+++ src/main/java/com/example/hms/HMS/specifications/GuestsSpecification.java
@@ -10,23 +10,31 @@ import java.util.List;
 public class GuestsSpecification {
 
     public static Specification<Guests> search(
+            String query,
             String name,
             String email,
-            String phone
-    ) {
-        return (root, query, cb) -> {
+            String phone) {
+        return (root, querySq, cb) -> {
 
             List<Predicate> predicates = new ArrayList<>();
+
+            if (query != null && !query.isBlank()) {
+                String queryLike = "%" + query.toLowerCase() + "%";
+                Predicate firstName = cb.like(cb.lower(root.get("firstName")), queryLike);
+                Predicate lastName = cb.like(cb.lower(root.get("lastName")), queryLike);
+                Predicate emailPred = cb.like(cb.lower(root.get("email")), queryLike);
+                Predicate phonePred = cb.like(root.get("phoneNumber"), "%" + query + "%");
+
+                predicates.add(cb.or(firstName, lastName, emailPred, phonePred));
+            }
 
             if (name != null && !name.isBlank()) {
                 Predicate firstName = cb.like(
                         cb.lower(root.get("firstName")),
-                        "%" + name.toLowerCase() + "%"
-                );
+                        "%" + name.toLowerCase() + "%");
                 Predicate lastName = cb.like(
                         cb.lower(root.get("lastName")),
-                        "%" + name.toLowerCase() + "%"
-                );
+                        "%" + name.toLowerCase() + "%");
                 predicates.add(cb.or(firstName, lastName));
             }
 
@@ -34,19 +42,15 @@ public class GuestsSpecification {
                 predicates.add(
                         cb.like(
                                 cb.lower(root.get("email")),
-                                "%" + email.toLowerCase() + "%"
-                        )
-                );
+                                "%" + email.toLowerCase() + "%"));
             }
 
             if (phone != null && !phone.isBlank()) {
                 predicates.add(
-                        cb.like(root.get("phoneNumber"), "%" + phone + "%")
-                );
+                        cb.like(root.get("phoneNumber"), "%" + phone + "%"));
             }
 
             return cb.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
-
