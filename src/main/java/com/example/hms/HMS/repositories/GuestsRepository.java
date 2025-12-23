092588319ff8cb0ab9f383a6d0fc3afc4ebbf622
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-23 05:45:31 +0530

    GuestsRepository

--- src/main/java/com/example/hms/HMS/repositories/GuestsRepository.java
+++ src/main/java/com/example/hms/HMS/repositories/GuestsRepository.java
@@ -2,8 +2,9 @@ package com.example.hms.HMS.repositories;
 
 import com.example.hms.HMS.entities.Guests;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GuestsRepository extends JpaRepository<Guests , Long> {
+public interface GuestsRepository extends JpaRepository<Guests , Long>, JpaSpecificationExecutor<Guests> {
 }
