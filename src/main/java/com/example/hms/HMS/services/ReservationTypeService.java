0791f205838c9e4ed32f732ec77f3a4afb0a56c5
Delojan1808 <121038216+Delojan1808@users.noreply.github.com>
2025-12-23 23:12:20 +0530

    reservation type edit services

--- src/main/java/com/example/hms/HMS/services/ReservationTypeService.java
+++ src/main/java/com/example/hms/HMS/services/ReservationTypeService.java
@@ -3,7 +3,7 @@ package com.example.hms.HMS.services;
 import com.example.hms.HMS.dtos.requests.ReservationTypeRequestDto;
 import com.example.hms.HMS.dtos.responses.ReservationTypeResponseDto;
 
-import com.example.hms.HMS.dtos.responses.ReservationTypeResponseDto;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -14,4 +14,6 @@ public interface ReservationTypeService {
     ReservationTypeResponseDto createReservationType(ReservationTypeRequestDto requestDto);
 
     Page<ReservationTypeResponseDto> fetchAllReservationType(Pageable pageSize);
+
+    ReservationTypeResponseDto updateReservationType( Long id, ReservationTypeRequestDto reservationTypeRequestDto);
 }
