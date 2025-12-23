1133f05122058c5c519bd6f167eac725f7b70b7b
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-23 05:44:53 +0530

    GuestsMapper

--- src/main/java/com/example/hms/HMS/mappers/GuestsMapper.java
+++ src/main/java/com/example/hms/HMS/mappers/GuestsMapper.java
@@ -4,9 +4,11 @@ import com.example.hms.HMS.dtos.responses.GuestsResponseDto;
 import com.example.hms.HMS.entities.Guests;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface GuestsMapper {
     Guests toDto(GuestsResponseDto guestsResponseDto);
     GuestsResponseDto toEntity(Guests guests);
-
+    List<GuestsResponseDto> toDtoList(List<Guests> guests);
 }
