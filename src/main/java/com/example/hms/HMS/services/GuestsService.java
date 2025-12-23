164e6c978c14f6b116f4aceebbf30a16184a5680
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-23 06:32:45 +0530

    GuestsService

--- src/main/java/com/example/hms/HMS/services/GuestsService.java
+++ src/main/java/com/example/hms/HMS/services/GuestsService.java
@@ -3,6 +3,7 @@ package com.example.hms.HMS.services;
 import com.example.hms.HMS.dtos.responses.GuestsResponseDto;
 import jakarta.validation.Valid;
 
+import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface GuestsService {
@@ -12,5 +13,7 @@ public interface GuestsService {
     boolean deleteManageGuests(@Valid Long id);
 
     List<GuestsResponseDto> searchGuests(String query, String name, String email, String phone);
+
+    Page<GuestsResponseDto> getAllGuests(int page, int size);
 
 }
