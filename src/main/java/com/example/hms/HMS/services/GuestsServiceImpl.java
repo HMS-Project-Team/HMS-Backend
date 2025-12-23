01c616eebd1e41bd153cd15fd51e79432e0d0841
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-23 05:46:14 +0530

    GuestsServiceImpl

--- src/main/java/com/example/hms/HMS/services/GuestsServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/GuestsServiceImpl.java
@@ -5,23 +5,25 @@ import com.example.hms.HMS.entities.Guests;
 import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
 import com.example.hms.HMS.mappers.GuestsMapper;
 import com.example.hms.HMS.repositories.GuestsRepository;
+import com.example.hms.HMS.specifications.GuestsSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class GuestsServiceImpl implements GuestsService{
+public class GuestsServiceImpl implements GuestsService {
 
     private final GuestsRepository guestsRepository;
     private final GuestsMapper guestsMapper;
 
-
     @Override
-    public GuestsResponseDto getGuestsById(long id){
-        Guests guest =guestsRepository.findById(id)
-                .orElseThrow(()->new RuntimeException("\"Guest not found with id: " + id));
+    public GuestsResponseDto getGuestsById(long id) {
+        Guests guest = guestsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("\"Guest not found with id: " + id));
 
-        return guestsMapper.toEntity(guest) ;
+        return guestsMapper.toEntity(guest);
     }
 
     @Override
@@ -31,4 +33,20 @@ public class GuestsServiceImpl implements GuestsService{
         guestsRepository.deleteById(id);
         return true;
     }
+
+    @Override
+    public List<GuestsResponseDto> searchGuests(
+            String name,
+            String email,
+            String phone) {
+        List<Guests> guests = guestsRepository.findAll(
+                GuestsSpecification.search(name, email, phone));
+
+        if (guests.isEmpty()) {
+            throw new ResourceNotFoundException("No guests found matching the search criteria.");
+        }
+
+        return guestsMapper.toDtoList(guests);
+    }
+
 }
