eb7d386b6be154f2c8569a047afb7af38ec13a7e
unknown <vijayapremanath@gmail.com>
2025-12-26 23:54:32 +0530

    RoomTypeServiceImpl

--- src/main/java/com/example/hms/HMS/services/RoomTypeServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/RoomTypeServiceImpl.java
@@ -1,7 +1,27 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
+import com.example.hms.HMS.entities.RoomType;
+import com.example.hms.HMS.mappers.RoomTypeMapper;
+import com.example.hms.HMS.repositories.RoomTypeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RoomTypeServiceImpl implements RoomTypeService {
+    private final RoomTypeRepository roomTypeRepository;
+    private final RoomTypeMapper roomTypeMapper;
+
+    @Override
+    public RoomTypeResponseDto getRoomTypeById(Long id) {
+
+        RoomType roomType = roomTypeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(
+                        "RoomType not found with id : " + id
+                ));
+
+        return roomTypeMapper.toDto(roomType);
+    }
+
 }
