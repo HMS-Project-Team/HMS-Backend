7b2ebd261b22216b6002c1bc2491174db85421e8
unknown <vijayapremanath@gmail.com>
2025-12-26 23:53:16 +0530

    RoomTypeService

--- src/main/java/com/example/hms/HMS/services/RoomTypeService.java
+++ src/main/java/com/example/hms/HMS/services/RoomTypeService.java
@@ -1,4 +1,7 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
+
 public interface RoomTypeService {
+    RoomTypeResponseDto getRoomTypeById(Long id);
 }
