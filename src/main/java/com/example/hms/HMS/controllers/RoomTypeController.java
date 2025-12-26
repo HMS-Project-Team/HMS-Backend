c6b50280a5a3c7ebea4fbf1ddc678789f24db9c2
unknown <vijayapremanath@gmail.com>
2025-12-26 23:52:45 +0530

    RoomTypeController

--- src/main/java/com/example/hms/HMS/controllers/RoomTypeController.java
+++ src/main/java/com/example/hms/HMS/controllers/RoomTypeController.java
@@ -1,7 +1,12 @@
 package com.example.hms.HMS.controllers;
 
+import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
+import com.example.hms.HMS.services.RoomTypeService;
 import com.example.hms.HMS.utils.EndpointBundle;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(EndpointBundle.ROOMTYPE)
 @RequiredArgsConstructor
 public class RoomTypeController {
+    private final RoomTypeService roomTypeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoomTypeResponseDto> getRoomTypeById(@PathVariable Long id) {
+        return ResponseEntity.ok(roomTypeService.getRoomTypeById(id));
+    }
 }
