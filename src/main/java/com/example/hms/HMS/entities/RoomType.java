322dcfa2e6d4f2bc54169a0a84247d4006981948
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-26 20:49:02 +0530

    fixed RoomType.java

--- src/main/java/com/example/hms/HMS/entities/RoomType.java
+++ src/main/java/com/example/hms/HMS/entities/RoomType.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public class RoomType extends DateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
