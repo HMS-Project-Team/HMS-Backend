b43ebd9e7ddbe514be080bbf7845771fd2a2d7aa
unknown <vijayapremanath@gmail.com>
2025-12-27 10:11:09 +0530

    Merge RoomTypeService

--- src/main/java/com/example/hms/HMS/services/RoomTypeService.java
+++ src/main/java/com/example/hms/HMS/services/RoomTypeService.java
@@ -2,9 +2,6 @@ package com.example.hms.HMS.services;
 
 import com.example.hms.HMS.dtos.requests.RoomTypeRequestDto;
 import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
-
-import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
-
 public interface RoomTypeService {
     RoomTypeResponseDto createRoomType(RoomTypeRequestDto roomTypeRequestDto);
     RoomTypeResponseDto getRoomTypeById(Long id);
