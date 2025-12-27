3a52c6f7dbcbcc222dc589ee90426d56ae6d70b7
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-27 10:51:49 +0530

    update child policy PoliciesServiceImpl

--- src/main/java/com/example/hms/HMS/services/PoliciesServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/PoliciesServiceImpl.java
@@ -1,7 +1,32 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.dtos.requests.PoliciesRequestDto;
+import com.example.hms.HMS.dtos.responses.PoliciesResponseDto;
+import com.example.hms.HMS.entities.Policies;
+import com.example.hms.HMS.enums.PolicyType;
+import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
+import com.example.hms.HMS.mappers.PoliciesMapper;
+import com.example.hms.HMS.repositories.PoliciesRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PoliciesServiceImpl implements PoliciesService {
+    private final PoliciesMapper policiesMapper;
+    private final PoliciesRepository policiesRepository;
+
+    @Override
+    public PoliciesResponseDto updateChildPolicy(Long id, PoliciesRequestDto policiesRequestDto) {
+        Policies policy = policiesRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Policy not found for id : " + id));
+
+        if (policy.getType() != PolicyType.CHILD) {
+            throw new ResourceNotFoundException("Policy found but is not a Child Policy for id : " + id);
+        }
+
+        policiesMapper.updateEntityFromDto(policiesRequestDto, policy);
+        policy.setType(PolicyType.CHILD); // Ensure it stays as CHILD type
+        return policiesMapper.toDto(policiesRepository.save(policy));
+    }
 }
