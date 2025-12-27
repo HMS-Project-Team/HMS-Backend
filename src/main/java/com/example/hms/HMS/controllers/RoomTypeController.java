8acfe4456d497988fed144b8919925d187b4c4b4
unknown <vijayapremanath@gmail.com>
2025-12-27 09:52:50 +0530

    RoomTypeController

--- src/main/java/com/example/hms/HMS/controllers/RoomTypeController.java
+++ src/main/java/com/example/hms/HMS/controllers/RoomTypeController.java
@@ -1,9 +1,13 @@
 package com.example.hms.HMS.controllers;
 
 import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
 import com.example.hms.HMS.services.RoomTypeService;
 import com.example.hms.HMS.utils.EndpointBundle;
+import com.example.hms.HMS.utils.ResponseWrapper;
+import com.example.hms.HMS.utils.ValidationMessages;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomTypeController {
     private final RoomTypeService roomTypeService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<RoomTypeResponseDto> getRoomTypeById(@PathVariable Long id) {
-        return ResponseEntity.ok(roomTypeService.getRoomTypeById(id));
+    @GetMapping(EndpointBundle.ID)
+    public ResponseEntity<ResponseWrapper<RoomTypeResponseDto>> getRoomTypeById(@PathVariable Long id) {
+        RoomTypeResponseDto data = roomTypeService.getRoomTypeById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper<>(
+                RestApiResponseStatusCodes.OK.getCode(),
+                ValidationMessages.RETRIEVED_SUCCESSFULLY,
+                data));
     }
 }
