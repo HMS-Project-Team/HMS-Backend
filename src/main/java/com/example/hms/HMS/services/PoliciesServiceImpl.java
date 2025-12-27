05bf1c4b8f5c7b98db7ec08115c6524ec35c89a4
Hany11-11 <rs11hari@gmail.com>
2025-12-28 00:42:31 +0530

    serviceImpl merged

--- src/main/java/com/example/hms/HMS/services/PoliciesServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/PoliciesServiceImpl.java
@@ -4,10 +4,14 @@ import com.example.hms.HMS.dtos.requests.PoliciesRequestDto;
 import com.example.hms.HMS.dtos.responses.PoliciesResponseDto;
 import com.example.hms.HMS.entities.Policies;
 import com.example.hms.HMS.enums.PolicyType;
+import com.example.hms.HMS.exceptionHandlers.InvalidPageSizeException;
 import com.example.hms.HMS.exceptionHandlers.ResourceNotFoundException;
 import com.example.hms.HMS.mappers.PoliciesMapper;
 import com.example.hms.HMS.repositories.PoliciesRepository;
+import com.example.hms.HMS.utils.ValidationMessages;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,12 +25,31 @@ public class PoliciesServiceImpl implements PoliciesService {
         Policies policy = policiesRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Policy not found for id : " + id));
 
-        if (policy.getType() != PolicyType.CHILD) {
-            throw new ResourceNotFoundException("Policy found but is not a Child Policy for id : " + id);
+        if (!PolicyType.CHILD.equals(policy.getType())) {
+            throw new ResourceNotFoundException("Policy found, but it is not a Child Policy.");
+        }
+
+        if (policiesRequestDto.getType() != null && !PolicyType.CHILD.equals(policiesRequestDto.getType())) {
+            throw new IllegalArgumentException("Cannot change Policy Type to " + policiesRequestDto.getType() + " via this policy.");
         }
 
         policiesMapper.updateEntityFromDto(policiesRequestDto, policy);
         policy.setType(PolicyType.CHILD); // Ensure it stays as CHILD type
         return policiesMapper.toDto(policiesRepository.save(policy));
+    }
+
+    @Override
+    public Page<PoliciesResponseDto> getPolicies(PolicyType type, Pageable pageable) {
+        if (pageable.getPageNumber() < 0 || pageable.getPageSize() <= 0){
+            throw new InvalidPageSizeException("Invalid page or size value");
+        }
+
+        Page<Policies> policiesPage = policiesRepository.findByType(type, pageable);
+
+        if (policiesPage.isEmpty()){
+            throw new RuntimeException(ValidationMessages.NOT_FOUND);
+        }
+
+        return policiesPage.map(policiesMapper::toDto);
     }
 }
