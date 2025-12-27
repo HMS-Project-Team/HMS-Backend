4e37d1c01ae41ae101b8595074a80a70b2c21ed2
aaryanhp1 <aaryanhp1@gmail.com>
2025-12-27 19:57:29 +0530

    ADD AND DELETE API

--- src/main/java/com/example/hms/HMS/controllers/TaxController.java
+++ src/main/java/com/example/hms/HMS/controllers/TaxController.java
@@ -1,6 +1,8 @@
 package com.example.hms.HMS.controllers;
 
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
 import com.example.hms.HMS.utils.EndpointBundle;
+import com.example.hms.HMS.utils.ValidationMessages;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(EndpointBundle.TAX)
 @RequiredArgsConstructor
 public class TaxController {
+
+    private final TaxService taxService;
+
+    @PostMapping(EndpointBundle.ADD)
+    public ResponseWrapper<TaxResponseDto> addTax(@Valid @RequestBody TaxRequestDto requestDto) {
+        return new ResponseWrapper<>(
+                RestApiResponseStatusCodes.CREATED.getCode(),
+                ValidationMessages.SAVED_SUCCESSFULLY,
+                taxService.addTax(requestDto)
+        );
+    }
+
+    @DeleteMapping(EndpointBundle.ID)
+    public ResponseWrapper<Void> deleteTax(@PathVariable Long id) {taxService.deleteTax(id);
+        return new ResponseWrapper<>(
+                RestApiResponseStatusCodes.OK.getCode(),
+                ValidationMessages.DELETED_SUCCESSFULLY,
+                null
+        );
+    }
 }
--- src/main/java/com/example/hms/HMS/services/TaxServiceImpl.java
+++ src/main/java/com/example/hms/HMS/services/TaxServiceImpl.java
@@ -1,7 +1,28 @@
 package com.example.hms.HMS.services;
 
+import com.example.hms.HMS.utils.ValidationMessages;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TaxServiceImpl implements TaxService {
+    private TaxRepository taxRepository;
+    private  TaxMapper taxMapper;
+
+
+    @Override
+    public TaxResponseDto addTax(TaxRequestDto requestDto) {
+        Tax tax = taxMapper.toEntity(requestDto);
+        Tax savedTax = taxRepository.save(tax);
+        return taxMapper.toDto(savedTax);
+    }
+
+    @Override
+    public void deleteTax(Long id) {
+        if (!taxRepository.existsById(id)) {
+            throw new EntityNotFoundException(ValidationMessages.NOT_FOUND);
+        }
+        taxRepository.deleteById(id);
+    }
 }
