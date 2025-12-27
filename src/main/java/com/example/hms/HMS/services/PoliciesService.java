a53a10f0ff92c69db1136064b8aea8dfa92946b9
Hany11-11 <rs11hari@gmail.com>
2025-12-28 00:42:13 +0530

    service merged

--- src/main/java/com/example/hms/HMS/services/PoliciesService.java
+++ src/main/java/com/example/hms/HMS/services/PoliciesService.java
@@ -2,7 +2,11 @@ package com.example.hms.HMS.services;
 
 import com.example.hms.HMS.dtos.requests.PoliciesRequestDto;
 import com.example.hms.HMS.dtos.responses.PoliciesResponseDto;
+import com.example.hms.HMS.enums.PolicyType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PoliciesService {
     PoliciesResponseDto updateChildPolicy(Long id, PoliciesRequestDto policiesRequestDto);
+    Page<PoliciesResponseDto> getPolicies(PolicyType type, Pageable pageable);
 }
