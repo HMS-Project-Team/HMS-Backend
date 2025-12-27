479ca6d77b34f3f3f8a96eb911276d6a758fd6b2
Hany11-11 <rs11hari@gmail.com>
2025-12-27 09:41:03 +0530

    Rommtype Controller edited

--- src/main/java/com/example/hms/HMS/controllers/RoomTypeController.java
+++ src/main/java/com/example/hms/HMS/controllers/RoomTypeController.java
@@ -1,7 +1,19 @@
 package com.example.hms.HMS.controllers;
 
+import com.example.hms.HMS.dtos.requests.RoomTypeRequestDto;
+import com.example.hms.HMS.dtos.responses.RoomTypeResponseDto;
+import com.example.hms.HMS.enums.RestApiResponseStatusCodes;
+import com.example.hms.HMS.services.RoomTypeService;
 import com.example.hms.HMS.utils.EndpointBundle;
+import com.example.hms.HMS.utils.ResponseWrapper;
+import com.example.hms.HMS.utils.ValidationMessages;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(EndpointBundle.ROOMTYPE)
 @RequiredArgsConstructor
 public class RoomTypeController {
+    private final RoomTypeService roomTypeService;
+
+    @PostMapping(EndpointBundle.CREATE_ROOMTYPE)
+    public ResponseEntity<ResponseWrapper<RoomTypeResponseDto>> createRoomType(@Valid @RequestBody RoomTypeRequestDto roomTypeRequestDto){
+        RoomTypeResponseDto newRoomType = roomTypeService.createRoomType(roomTypeRequestDto);
+
+        if (newRoomType != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper<>(
+                    RestApiResponseStatusCodes.CREATED.getCode(),
+                    ValidationMessages.SAVED_SUCCESSFULLY,
+                    newRoomType
+            ));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseWrapper<>(
+                    RestApiResponseStatusCodes.BAD_REQUEST.getCode(),
+                    ValidationMessages.BAD_REQUEST,
+                    null
+            ));
+        }
+    }
 }
