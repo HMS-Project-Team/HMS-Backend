ca174e03e0ef76c5adfa2cf07580e98e5562bbdc
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2025-12-27 23:44:39 +0530

    Updated PoliciesService

--- src/main/java/com/example/hms/HMS/services/PoliciesService.java
+++ src/main/java/com/example/hms/HMS/services/PoliciesService.java
@@ -2,7 +2,9 @@ package com.example.hms.HMS.services;
 
 import com.example.hms.HMS.dtos.requests.PoliciesRequestDto;
 import com.example.hms.HMS.dtos.responses.PoliciesResponseDto;
+import jakarta.validation.Valid;
 
 public interface PoliciesService {
     PoliciesResponseDto updateChildPolicy(Long id, PoliciesRequestDto policiesRequestDto);
+    PoliciesResponseDto getChildPolicyById(@Valid Long id);
 }
