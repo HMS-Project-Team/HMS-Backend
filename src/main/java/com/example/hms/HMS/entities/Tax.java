2d01910f4f1d06af9b58ba400c9471fb59282ace
Sujeevan8300 <sujeevan8300@gmail.com>
2025-12-28 20:53:36 +0530

    Tax

--- src/main/java/com/example/hms/HMS/entities/Tax.java
+++ src/main/java/com/example/hms/HMS/entities/Tax.java
@@ -1,6 +1,7 @@
 package com.example.hms.HMS.entities;
 
 import com.example.hms.HMS.enums.Status;
+import com.example.hms.HMS.enums.TaxType;
 import com.example.hms.HMS.utils.DateAudit;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -19,9 +20,11 @@ public class Tax extends DateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String name;
     private BigDecimal rate;
-    private String type;
+    @Enumerated(EnumType.STRING)
+    private TaxType type;
 
     @Enumerated(EnumType.STRING)
     private Status status;
