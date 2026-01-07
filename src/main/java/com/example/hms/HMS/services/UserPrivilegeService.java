4ba3214017474450ae7e53a1c78a3b0d33ec4ead
kokilannithusan <165774476+kokilannithusan@users.noreply.github.com>
2026-01-07 16:20:39 +0530

    updated user privilage service

--- src/main/java/com/example/hms/HMS/services/UserPrivilegeService.java
+++ src/main/java/com/example/hms/HMS/services/UserPrivilegeService.java
@@ -7,5 +7,6 @@ import com.example.hms.HMS.dtos.responses.UserPrivilegeResponseDto;
 import java.util.List;
 
 public interface UserPrivilegeService {
+    List<UserPrivilegeResponseDto> getUserPrivileges(Long userId);
 
 }
