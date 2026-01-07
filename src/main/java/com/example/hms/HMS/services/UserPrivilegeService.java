eba0306f4a3c53adeb2d9d51b2ea34a4f1fc6bf7
unknown <vijayapremanath@gmail.com>
2026-01-07 16:19:34 +0530

    add UserPrivilegeService

--- src/main/java/com/example/hms/HMS/services/UserPrivilegeService.java
+++ src/main/java/com/example/hms/HMS/services/UserPrivilegeService.java
@@ -7,5 +7,5 @@ import com.example.hms.HMS.dtos.responses.UserPrivilegeResponseDto;
 import java.util.List;
 
 public interface UserPrivilegeService {
-
+    UserPrivilegeResponseDto updateUserPrivilege(Long userId, UserPrivilegeRequestDto dto);
 }
