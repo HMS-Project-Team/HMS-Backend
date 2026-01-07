f91b58ba2c43960ad8fb5016af1e3270b8459c01
jeyakkanth <121349336+jeyakkanth@users.noreply.github.com>
2026-01-07 16:56:46 +0530

    Updated HotelPrivilegeService

--- src/main/java/com/example/hms/HMS/services/HotelPrivilegeService.java
+++ src/main/java/com/example/hms/HMS/services/HotelPrivilegeService.java
@@ -7,5 +7,6 @@ import com.example.hms.HMS.dtos.responses.HotelPrivilegeResponseDto;
 import java.util.List;
 
 public interface HotelPrivilegeService {
+    void addHotelPrivileges(Long hotelId, BulkPrivilegeAssignmentDto dto);
 
 }
