27648156a4d6933b46bf5ce86e1a3e0bff5c3656
Hany11-11 <rs11hari@gmail.com>
2026-01-07 16:11:24 +0530

    edited controller

--- src/main/java/com/example/hms/HMS/controllers/SettingsRolePrivilegeController.java
+++ src/main/java/com/example/hms/HMS/controllers/SettingsRolePrivilegeController.java
@@ -20,7 +20,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SettingsRolePrivilegeController {
 
-        private final RolePrivilegeService rolePrivilegeService;
+    private final RolePrivilegeService rolePrivilegeService;
 
+    @PutMapping(EndpointBundle.ROLE_ID)
+    @RequirePrivilege(privilege = "/settings/hotel-role-privileges", type = PrivilegeType.WRITE_ACCESS)
+    public ResponseEntity<ResponseWrapper<RolePrivilegeResponseDto>> updateRolePrivilege(
+            @PathVariable Long roleId,
+            @RequestBody RolePrivilegeRequestDto dto) {
+        return ResponseEntity.ok(new ResponseWrapper<>(
+                RestApiResponseStatusCodes.OK.getCode(),
+                RestApiResponseStatusCodes.OK.getMessage(),
+                rolePrivilegeService.updateRolePrivilege(roleId, dto)));
+    }
 
 }
